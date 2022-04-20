96b2728cfea7e7c18c2f1c347c3740f3a5be6037
Yahya Gilany <gilanyym@ucmail.uc.edu>
2022-04-20 13:57:10 -0400

    Deploy website - based on 34477f936c41a9d2216efa224a418eab46c30254

---
+++ assets/js/37a0ce75.ce335d83.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksite=self.webpackChunksite||[]).push([[6787],{96928:function(s){s.exports=JSON.parse('{"plugin":{"name":"docusaurus-plugin-content-docs","id":"instructor"}}')}}]);
