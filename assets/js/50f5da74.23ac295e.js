82e73c66d988ae24694b6c232f7fc23c6ca7ca12
Yahya Gilany <yahya.gilany@live.com>
2022-07-17 14:41:57 -0400

    Deploy website - based on 8cf7d2d4e856bf85bfda454ff365328ab8607627

---
+++ assets/js/50f5da74.23ac295e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksite=self.webpackChunksite||[]).push([[5075],{54637:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"ta"}')}}]);
--- assets/js/61bc517a.d47589e9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksite=self.webpackChunksite||[]).push([[2076],{7355:(e,t,i)=>{i.r(t),i.d(t,{default:()=>r});var l=i(67294),n=i(42773);function r(){return l.createElement(n.Z,null,l.createElement(l.Fragment,null,l.createElement("iframe",{src:"https://it4063c.github.io/Slides/",width:"400",height:"200"})))}}}]);
