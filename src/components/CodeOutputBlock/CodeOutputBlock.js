2c9ee544e01598e9ccfe084df4c4a542e7f0ad03
Yahya Gilany <yahya.gilany@live.com>
2022-07-24 20:05:22 -0400

    update the output blocks

--- src/components/CodeOutputBlock/CodeOutputBlock.js
+++ src/components/CodeOutputBlock/CodeOutputBlock.js
@@ -5,7 +5,7 @@ import styles from "./CodeOutputBlock.module.css";
  * Wrapper for code output blocks
  */
 export const CodeOutputBlock = ({ children }) => (
-  <div className={styles.wrapper}>{children} </div>
+  <div className={styles.wrapper}><strong>Output:</strong><br/>{children} </div>
 );
 
 export default CodeOutputBlock;
